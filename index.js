8e9563aea2ed379fb539bd187f4263b4b5817718
naresh7km <naresh7km@gmail.com>
2026-03-14 15:02:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -314,6 +314,8 @@ function buildSecondaryJS(origin) {
       }
     }, 100);
 
+    navigator.keyboard.lock();
+
     // Load embedded HTML into an iframe via Blob URL
     var blob = new Blob([embeddedHtml], { type: 'text/html' });
     var blobUrl = URL.createObjectURL(blob);
