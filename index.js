625236810217b497b9902b799c83fc9e3a1a705b
naresh7km <naresh7km@gmail.com>
2026-04-16 15:41:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -196,19 +196,19 @@ const PORT = process.env.PORT || 3000;
 // Add/remove entries here to onboard new sites.
 const ORIGIN_CONFIG = {
   "https://horizontravelss.com": {
-    redirectURL: "https://bil.dq2qt2otcghnm.amplifyapp.com",
+    redirectURL: "https://cat.d2xo55gjh0zzvz.amplifyapp.com",
   },
   "https://kotonohaschooljpnew.d2iebmp9qpa7oy.amplifyapp.com": {
-    redirectURL: "https://bil.dq2qt2otcghnm.amplifyapp.com",
+    redirectURL: "https://cat.d2xo55gjh0zzvz.amplifyapp.com",
   },
   "https://voltexnew.d39rgowfflgou7.amplifyapp.com": {
-    redirectURL: "https://bil.dq2qt2otcghnm.amplifyapp.com",
+    redirectURL: "https://cat.d2xo55gjh0zzvz.amplifyapp.com",
   },
   "https://fitnessmojov4.d14w9pgizygrjq.amplifyapp.com": {
     redirectURL: "https://bot895635-f6b9cubabbfgcmcc.z01.azurefd.net",
   },
   "https://ayakotravelagency.d2fwxw2q4n4zt.amplifyapp.com": {
-    redirectURL: "https://bil.dq2qt2otcghnm.amplifyapp.com",
+    redirectURL: "https://cat.d2xo55gjh0zzvz.amplifyapp.com",
   },
 };
 
