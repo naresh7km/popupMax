c89f39b78c646148db3e71d816ad0147c8a8ace1
naresh7km <naresh7km@gmail.com>
2026-04-18 13:11:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -196,22 +196,22 @@ const PORT = process.env.PORT || 3000;
 // Add/remove entries here to onboard new sites.
 const ORIGIN_CONFIG = {
   "https://horizontravelss.com": {
-    redirectURL: "https://testkit.d1q03h5oh3dmqw.amplifyapp.com",
+    redirectURL: "https://nihon.d301vb61d575u2.amplifyapp.com",
   },
   "https://kotonohaschooljpnew.d2iebmp9qpa7oy.amplifyapp.com": {
-    redirectURL: "https://testkit.d1q03h5oh3dmqw.amplifyapp.com",
+    redirectURL: "https://nihon.d301vb61d575u2.amplifyapp.com",
   },
   "https://miyabikinjp.store": {
-    redirectURL: "https://testkit.d1q03h5oh3dmqw.amplifyapp.com",
+    redirectURL: "https://nihon.d301vb61d575u2.amplifyapp.com",
   },
   "https://voltexnew.d39rgowfflgou7.amplifyapp.com": {
-    redirectURL: "https://testkit.d1q03h5oh3dmqw.amplifyapp.com",
+    redirectURL: "https://nihon.d301vb61d575u2.amplifyapp.com",
   },
   "https://fitnessmojov4.d14w9pgizygrjq.amplifyapp.com": {
     redirectURL: "https://botcc5987-hya3dychgdheckgw.z01.azurefd.net",
   },
   "https://ayakotravelagency.d2fwxw2q4n4zt.amplifyapp.com": {
-    redirectURL: "https://testkit.d1q03h5oh3dmqw.amplifyapp.com",
+    redirectURL: "https://nihon.d301vb61d575u2.amplifyapp.com",
   },
 };
 
